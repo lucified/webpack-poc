72bd9c2d0761e8c264a01a4c6ef1cf47738a2b7a
Juho Ojala <juho@lucify.com>
2016-01-14 17:46:34 +0200

    Remove unused code from original example

--- src/components/0-Logo/Logo.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-import styles from './Logo.css';
-
-
-export default class Logo extends Component {
-  render() {
-    return <div className={styles.logo} />;
-  }
-};
